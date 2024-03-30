75219e2cd8abeddc163ad70dddd2efeab5e80206
abdulkareemm <eng.kreem.96@gmail.com>
2024-03-30 13:04:33 +0300

    card component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export {default as Navbar} from "./navbar/Navbar"
 export { default as SearchBar } from "./searchBar/SearchBar";
+export { default as Card } from "./card/Card";
+
 
--- src/routes/list/List.jsx
+++ src/routes/list/List.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import {Card} from "../../components";
+import "./list.scss"
+import { listData } from "../../lib/data";
 
 const List = () => {
+  const data = listData;
   return (
-    <div>List</div>
-  )
-}
+    <div className="listPage">
+      <div className="listContainer">
+        <div className="wrapper">
+          {data.map((item) => (
+            <Card key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+      <div className="mapContainer">{/* <Map items={data} /> */}</div>
+    </div>
+  );
+};
 
-export default List
+export default List;

55aabe93f48bfb3ee1573e6c7fbf459de8379c4e
abdulkareemm <eng.kreem.96@gmail.com>
2024-03-31 04:17:19 +0300

    fix logo size

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
       <div className="left">
         <a href="/" className="logo">
           <img src="/logo.png" alt="" />
-          <span>LamaEstate</span>
+          <span>Estates</span>
         </a>
         <a href="/">Home</a>
         <a href="/">About</a>

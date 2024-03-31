2c85b27ea6207ff5597e97b246324f2eac73d39b
abdulkareemm <eng.kreem.96@gmail.com>
2024-03-31 03:52:55 +0300

    add login page

---
+++ src/routes/login/Login.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import "./login.scss"
+
+const Login = () => {
+ return (
+   <div className="login">
+     <div className="formContainer">
+       <form>
+         <h1>Welcome back</h1>
+         <input name="username" type="text" placeholder="Username" />
+         <input name="password" type="password" placeholder="Password" />
+         <button>Login</button>
+         <Link to="/register">{"Don't"} you have an account?</Link>
+       </form>
+     </div>
+     <div className="imgContainer">
+       <img src="/bg.png" alt="" />
+     </div>
+   </div>
+ );
+}
+
+export default Login
